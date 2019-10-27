b4180d1932a9815b8dc41ac8d11bb5258d800d9d
slambert27 <samlambert616@gmail.com>
2019-10-26 21:10:41 -0400

    Initial Commit

---
+++ LineGraph/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LineGraph
+//
+//  Created by Sam Lambert on 10/26/19.
+//  Copyright © 2019 Sam Lambert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
